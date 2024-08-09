8994ba00556114b3a7cfb445c598d75a1554895f
Alan White <alan@onelonemonkey.com>
2024-08-09 15:12:48 +0100

    fix: images

--- src/content/blog/great-tofu-experiment.md
+++
@@ -1,9 +0,0 @@
----
-title: The Great Tofu Experiment
-description: A badly done deep dive into trying to make tofu taste good
-pubDate: Jul 08 2022
-heroImage: ""
-featureImage: ""
----
-
-<iframe width="560" height="315" src="https://www.youtube.com/embed/BHuH8f43QVw?si=WQjWx4hM_2Us_M0D" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
--- src/content/config.ts
+++ src/content/config.ts
@@ -3,15 +3,15 @@ import { defineCollection, z } from "astro:content";
 const blog = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    featureImage: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: z.string().optional(),
+    }),
 });
 
 const recipe = defineCollection({
