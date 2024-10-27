74fefa8a06760fc9760309804f86dc0d547e5e27
Alan White <alan@onelonemonkey.com>
2024-10-27 09:12:31 +0000

    chore: configure typescript to not throow errors on class in astro templates

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,8 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://alanandzack.netlify.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  // Ensure TypeScript is aware of Astro's environment
+  typescript: {
+    strict: true,
+  },
 });
